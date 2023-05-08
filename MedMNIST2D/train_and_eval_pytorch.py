876dcbf9036d388bd3945ed0e4ff6f6cef832485
nimrobotics <aakash.iitt@gmail.com>
2023-05-07 23:05:09 -0500

    write params to logs

--- MedMNIST2D/train_and_eval_pytorch.py
+++ MedMNIST2D/train_and_eval_pytorch.py
@@ -221,7 +221,12 @@ def main(data_flag, output_root, num_epochs, gpu_ids, batch_size, download, mode
     else:
         test_log = ''
 
-    log = '%s\n' % (data_flag) + train_log + val_log + test_log
+    log = '%s\n' % (data_flag) + train_log + val_log + test_log + \
+            'task: %s' % (task) + \
+            'batch_size: %d  lr: %f  num_epochs: %d  optimizer: %s  optimizer_type: %s  milestones: %s  gamma: %f\n' % \
+            (batch_size, lr, num_epochs, optimizer, optimizer_type, milestones, gamma) + \
+            'rotation: %d  translation: %d  scale: %d  libauc_loss: %d  \n' % \
+            (rotation, translation, scale, libauc_loss) + \
     print(log)
             
     with open(os.path.join(output_root, '%s_log.txt' % (data_flag)), 'a') as f:
